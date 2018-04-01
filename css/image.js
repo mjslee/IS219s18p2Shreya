05fdced8e026a5f8d869ee32f3fdd66d8fe44d0b
Shreya Patel <sap87>
2018-04-01 12:05:42 -0400

    added a new code the the js file

--- css/image.js
+++ css/image.js
@@ -1,16 +1,10 @@
-
-$(document).ready(function() {
-
-    function displayPhotos(data) {
-      var photoHTML = "";
-      $.each(data,function(i,photo) {
-        photoHTML += '<div class="picbox"><figure><img src="' + photo.url + '" class="frame"><figcaption>' + photo.description + '</figcaption></figure></div>';
-          
-      }); // end each
-      
-      $('#photos').html(photoHTML);
-    };
-
-    $.getJSON("mscphotos.json", displayPhotos);
-    
-}); // end ready
+$(document).ready(function () {
+    var jsonURL = "images.json";
+    $.getJSON(jsonURL, function (json){
+        var imgList= "";
+        $.each(json.images, function () {
+            imgList += '<li><img src= "' + this.imgPath + '"></li>';
+        });
+        $('#dvimages').append(imgList);
+    });
+});
