6a89e672c9dec261bc1fbaa9f93c065e73945719
Shreya Patel <sap87>
2018-03-31 20:27:58 -0400

    created a image.js javascript file

---
+++ css/image.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+    function displayPhotos(data) {
+      var photoHTML = "";
+      $.each(data,function(i,photo) {
+        photoHTML += '<div class="picbox"><figure><img src="' + photo.url + '" class="frame"><figcaption>' + photo.description + '</figcaption></figure></div>';
+          
+      }); // end each
+      
+      $('#photos').html(photoHTML);
+    };
+
+    $.getJSON("mscphotos.json", displayPhotos);
+    
+}); // end ready
