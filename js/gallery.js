4e79e4daf8fce0fffd9cb06f1f09875db5ccb010
Shreya Patel <sap87>
2018-04-02 11:06:54 -0400

    updated the txt

--- js/gallery.js
+++ js/gallery.js
@@ -117,20 +117,6 @@ function makeGalleryImageOnloadCallback(galleryImage) {
   }
 }
 
-function getQueryParams(qs) {
-    qs = qs.split("+").join(" ");
-    var params = {},
-        tokens,
-        re = /[?&]?([^=]+)=([^&]*)/g;
-    while (tokens = re.exec(qs)) {
-        params[decodeURIComponent(tokens[1])]
-            = decodeURIComponent(tokens[2]);
-}
-    return params;
-}
-var $_GET = getQueryParams(document.location.search);
-console.log($_GET["json"]); // would output "John"
-
 
 
 $(document).ready( function() {
